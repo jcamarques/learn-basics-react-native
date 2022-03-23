678cd6ffa78f689b667446b7fb4424cff5db0079
Joao Celso <joaocelso01@gmail.com>
2022-03-23 10:18:25 -0300

    Variaveis simples

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+import { Text } from 'react-native';
 
-const App = () => {
+const getFullName = (firstName, secondName, thirdName) => {
+  return firstName + " " + secondName + " " + thirdName;
+}
+
+const Cat = () => {
   return (
-    <ScrollView>
-      <Text>Some text</Text>
-      <View>
-        <Text>Some more text</Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{ width: 200, height: 200 }}
-        />
-      </View>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
-        defaultValue="You can type in me"
-      />
-    </ScrollView>
+    <Text>
+      Hello, I am {getFullName("Rum", "Tum", "Tugger")}!
+    </Text>
   );
 }
 
-export default App;
+export default Cat;
